8dc626e25404634960ef4fe5dadf55f1d2683621
Konstantin Trokhimets <85570518+Trohimets@users.noreply.github.com>
2022-04-11 21:54:46 +0300

    Update settings.py
    Добавлен STATIC_ROOT

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
